534a06e30b425023440026bedd37509f5e21f81a
Tomas Quispe <tomasbquispe@gmail.com>
2023-03-12 00:56:09 -0300

    hice el ejercicio 4

--- js/ejercicio4.js
+++ js/ejercicio4.js
@@ -1,1 +1,14 @@
 //4- Realiza un script que pida números hasta que se pulse “cancelar”. Si no es un número deberá indicarse con un «alert» y seguir pidiendo números. Al salir con “cancelar” deberá indicarse la suma total de los números introducidos.
+let suma = 0;
+do{
+    let numero = prompt("ingrese un numero");
+    if (Number(numero) == numero) {
+        numero = Number(numero);
+        suma = suma + numero; 
+    }else{ 
+        if (numero !=undefined);
+        alert(numero + " No es un numero");
+    }    
+}while(confirm("ok/cancel"));
+document.write(suma);
+
