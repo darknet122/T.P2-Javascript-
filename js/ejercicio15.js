8a65aa49e3ee9d5c5b2dbce93c97810a8d17e1cd
Tomas Quispe <tomasbquispe@gmail.com>
2023-03-12 16:56:34 -0300

    hice el ejercicio 15

--- js/ejercicio15.js
+++ js/ejercicio15.js
@@ -1,1 +1,14 @@
 //15- Realiza un script que cuente el número de vocales que tiene un texto.
+let texto = prompt("Escriba un texto:");
+let numtext = texto.length;   
+texto = texto.toUpperCase();  
+let vocal;
+let contador = 0;
+let i;
+for (i = 0; i < numtext; i++) {
+    vocal = texto.charAt(i);   
+    if ((vocal == "A") || (vocal == "E") || (vocal == "I") || (vocal == "O") || (vocal == "U")) {
+        contador++;
+    }
+}
+document.write("Número de Vocales: " + contador + ".");
